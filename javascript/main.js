72abd6e58bd8c92c2979902ce50e2ed6554fe96f
lngdao <dtlong.2101@gmail.com>
2023-02-27 23:07:33 +0700

    update

---
+++ javascript/main.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+// Variable
+var a = 1;
+var b = 'abc';
+var c = 1.5;
+let d = [1, 2, 3, 4];
+const e = Math.PI;
+
+console.log(a, b, c, d, e);
+
+// Noi chuoi
+let string1 = 'dao';
+let string2 = 'long';
+let string3 = string1 + ' ' + string2;
+
+console.log(string3);
+
+console.log(`${string1} ${string2}`); // literal string
+
+// so sanh
+console.log(1 !== 2);
+
+// toan tu logic
+console.log(true || false);
+console.log(true && false);
+console.log(!true);
+
+// Kieu du lieu
+
+// Ham la gi
+// declaration function
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(1, 2));
+
+// expression function
+var sumFunction = function (num1, num2) {
+  console.log(num1 + num2);
+};
+
+sumFunction(2, 3);
+
+// arrow function
+const aArrowFunc = (num1, num2) => num1 + num2;
+
+console.log(aArrowFunc(4, 5));
+
+// BT1: Viet 1 ham hien thi thong tin ca nhan
+
+function showInformation(name, age, address) {
+  console.log(`name: ${name}\nage: ${age}\naddress: ${address}`);
+}
+
+showInformation('long dao', 22, 'Ha noi');
+
+// BT2: Viet ham tinh dien tich tam giac va hien thi ra man hinh canh1: , canh2: , dien tich:
+
+function dientich(cgv1, cgv2) {
+  const result = (cgv1 * cgv2 * 1) / 2;
+
+  console.log(`canh1: ${cgv1} canh2: ${cgv2}, dien tich: ${result}`);
+}
+
+dientich(20, 40);
+
+console.log(path.dirname(__filename));
